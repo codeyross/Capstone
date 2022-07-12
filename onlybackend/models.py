49013461f3f91a42e9a43b3a8114096848158d7c
Ross <ross.warren98@yahoo.com>
2022-07-12 11:04:46 -0400

    full crud working for user and recipe

---
+++ onlybackend/models.py
@@ -0,0 +1,34 @@
+
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    chef = models.CharField(max_length=500)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=500)
+    password = models.CharField(max_length=100)
+    user_permissions = models.CharField(max_length=1000)
+    profile_pic = models.TextField()
+    nationality = models.CharField(max_length=200)
+    about = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.chef
+#may need to add chef to recipe class for foreign key for proper link/naming
+
+class Recipes(models.Model):
+    user = models.ForeignKey(
+                    User, 
+                    on_delete=models.CASCADE, 
+                    related_name='recipes',
+                    )
+    recipe_name = models.CharField(max_length=1000, default='no recipe name')
+    picture = models.TextField()
+    recipe_description = models.CharField(max_length=1000, default='no recipe description')
+    ingredients = models.CharField(max_length=5000, default='no ingredients listed')
+    recipe = models.CharField(max_length=10000, default='no recipe listed')
+ 
+    def __str__(self):
+        return self.recipe_name
